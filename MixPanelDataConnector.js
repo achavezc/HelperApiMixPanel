e044c8c1569158d12f6d82babe9eaf785e649fa5
achavezc <anderson.chavezc@gmail.com>
2017-04-02 19:02:25 -0500

    add function getEvents

--- MixPanelDataConnector.js
+++ MixPanelDataConnector.js
@@ -3,17 +3,25 @@ module.exports = require('./lib/mixpanel');
 mixpanel = require('./lib/mixpanel');
 
 
-var  api_secret = '447e92839ec3755e52b7c559ebe15d31';
 
 var mx = new mixpanel({
-    api_secret: api_secret
+    api_secret: '447e92839ec3755e52b7c559ebe15d31'
 });
 
-mx.export_data({ from_date: '2017-02-11', to_date: '2017-03-11' }, function(res) {
-    res.on('data', function(event_object) {
-        console.dir(event_object);
-    });
-    res.on('end', function() {
-        console.log('All events have been retrieved');
-    });
-});
+lastUpdated = {
+  from_date : '2017-03-11',
+  to_date : '2017-03-12'
+}
+
+function getEvents(lastUpdated){
+    mx.export_data({ from_date: lastUpdated.from_date, to_date: lastUpdated.to_date }, function(res) {
+        res.on('data', function(event_object) {
+            console.dir(event_object);
+        });
+        res.on('end', function() {
+            console.log('All events have been retrieved');
+        });
+    })
+}
+
+getEvents(lastUpdated);
